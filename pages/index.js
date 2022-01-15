c4a1b67c7ed4389b0228025d862cf5a41b6a0b9d
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-15 19:03:21 +0700

    CSR

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import Image from 'next/image';
 import useSWR from 'swr';
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function Home({ json }) {
+export default function Home() {
   const { data, error } = useSWR(
-    'https://randomuser.me/api/?results=500',
+    'https://randomuser.me/api/?results=1000',
     fetcher
   );
 
   if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <div>
-      {json.results.map((result) => (
+      {data.results.map((result) => (
         <Image
           key={result.email}
           src={result.picture.large}
@@ -21,23 +22,6 @@ export default function Home({ json }) {
           height={100}
         />
       ))}
-      {data &&
-        data.results.map((result) => (
-          <Image
-            key={result.email}
-            src={result.picture.large}
-            alt={`${result.name.title} ${result.name.first} ${result.name.last}`}
-            width={100}
-            height={100}
-          />
-        ))}
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const res = await fetch(`https://randomuser.me/api/?results=1000`);
-  const json = await res.json();
-
-  return { props: { json } };
 }
